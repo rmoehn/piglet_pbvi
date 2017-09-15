f2d2d2c2800d46f7d002e2e5e1e59f15ccf7e96e
Richard Möhn <richard.moehn@posteo.de>
2017-09-15 10:14:54 +0900

    Add draft of the backup part of PBVI
    Probably it has a bunch of defects.

--- pbvi.py
+++ pbvi.py
@@ -1,10 +1,12 @@
+# -*- encoding: utf-8 -*-
+
 import collections
 
 import numpy as np
 
 
 def _gamma_ast(R):
-    return R.T.copy()
+    return R.T[:,None,:].copy()
     # Copy in order to reorder in memory.
 
 
@@ -15,12 +17,12 @@ def _tau(T):
     In::
 
         |S| x |A| x |S|
-        s     a     s'
+         s     a     s'
 
     Out::
 
         |A| x 1 x 1 x |S| x |S|  (1 for newaxis)
-        a             s'    s
+         a             s'    s
     """
     return np.moveaxis(T, [0,1,2], [2,0,1])[:,None,None,:].copy()
 
@@ -35,10 +37,47 @@ class PBVI(object):
 
 
     def gamma(self, V_):
-        prod1 = np.multiply(self.omega[:,:,None], V_,
-                            out=self._outs['gamma'].get('prod1'))
-        prod2 = np.multiply(self.tau, prod1,
-                            out=self._outs['gamma'].get('prod2'))
-        sum_s_ = np.sum(prod2, -1, out=self._outs['gamma'].get('sum_s_'))
-        return np.multiply(self.discount_gamma, sum_s_,
-                           out=self._outs['gamma'].get('result'))
+        l = self._outs['gamma']  # l for locals
+
+        l['prod1']   = np.multiply(self.omega[:,:,None], V_, out=l.get('prod1'))
+        l['prod2']   = np.multiply(self.tau, l['prod1'], out=l.get('prod2'))
+        l['sum_s_']  = np.sum(l['prod2'], -1, out=l.get('sum_s_'))
+        l['result']  = np.multiply(self.discount_gamma, l['sum_s_'],
+                                   out=l.get('result'))
+
+        return l['result']
+
+
+    def epsilon(self, B, gamma):
+        l = self._outs['epsilon']
+
+        # alpha · b for all a, o, b
+        l['crossprods']         = np.matmul(B, np.swapaxes(gamma, -1, -2),
+                                            out=l.get('crossprods'))
+        l['best_alpha_inds']    = np.argmax(l['crossprods'], -1,
+                                            out=l.get('best_alpha_inds'))
+
+        (n_as, n_os)    = gamma.shape[0:1]
+
+        # Credits: https://stackoverflow.com/questions/40357335/
+        #          numpy-how-to-get-a-max-from-an-argmax-result
+        best_per_o      = gamma[np.arange(n_as)[:,None,None],
+                                np.arange(n_os)[None,:,None],
+                                l['best_alpha_inds']]
+
+        # Gamma_a_b for each a and b
+        l['E'] = np.sum(best_per_o, 1, out=l.get('E'))  # not yet E
+        l['E'] += self.gamma_ast   # Now it's E.
+        # If this is slow, try E.swapaxes(…) += gamma_ast (gamma_ast unmodified)
+
+        return l['E']
+
+
+    def V(self, E, B):
+        l = self._outs['V']
+
+        l['values']     = np.matmul(E, B[:,:,None], out=l.get('prod'))
+        l['best_as']    = np.argmax(np.squeeze(l['values']), axis=1,
+                                    out=l.get('best_as'))
+
+        return E[np.arange(E.shape[0]), l['best_as']]
