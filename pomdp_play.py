000ac42dc05cbc46850944c617a119a3498f5841
Richard Möhn <richard.moehn@posteo.de>
2017-09-20 07:46:20 +0900

    pomdp_play: Add a bunch of stuff

--- pomdp_play.py
+++ pomdp_play.py
@@ -40,20 +40,48 @@ B = np.array([[0.4, 0.6],
               [0.2, 0.8],
               [0.8, 0.2]])
 
-for _ in xrange(2):
+b1 = np.linspace(0.1, 0.9, 8)
+B = np.stack([1 - b1, b1], axis=-1)
+
+for _ in xrange(9):
     gamma   = apbvi.gamma(V)
     epsilon = apbvi.epsilon(B, gamma)
     V       = apbvi.V(epsilon, B)
     print V
 
+fig, ax = plt.subplots()
+for v in V:
+    ax.plot([0, 1], v)
+plt.show()
+
 
 anpbvi = naive_pbvi.NaivePBVI(cR, cT, cO, discount_gamma)
 
-#nV = np.zeros((1,2), np.float64)
-nV = np.array([[0., 1.]])
+nV = np.zeros((1, 2), np.float64)
 
-for _ in xrange(2):
+for _ in xrange(0):
     ngamma = anpbvi.gamma(nV)
     nepsilon = anpbvi.epsilon(B, ngamma)
     nV = anpbvi.V(nepsilon, B)
+
+print "\nV\n", nV
+
+for i in xrange(0):
+    print "\n== Round %d ==" % i
+    print "GAMMA"
+    ngamma = anpbvi.gamma(nV)
+    print ngamma
+    gamma = apbvi.gamma(V)
+    print gamma
+
+    print "EPSILON"
+    nepsilon = anpbvi.epsilon(B, ngamma)
+    print nepsilon
+    epsilon = apbvi.epsilon(B, gamma)
+    print epsilon
+
+    print "V"
+    nV = anpbvi.V(nepsilon, B)
     print nV
+    V = apbvi.V(epsilon, B)
+    print V
