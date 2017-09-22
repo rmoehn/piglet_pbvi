fe516ddaa32c5aa03c36f7bb52d54f624d69d97a
Richard Möhn <richard.moehn@posteo.de>
2017-09-23 07:14:09 +0900

    verify_psi: Fix and extend

--- verify_psi.py
+++ verify_psi.py
@@ -8,7 +8,20 @@ def psi_samples(st, at1, T, omega, n_root=100):
     samples.
     """
     (n_s, n_o) = omega.shape[1:]
-    st1_samples = np.choice(n_s, size=n_root, replace=True, p=T[st, at1])
-    ot1_samples = [np.choice(n_o, size=n_root, replace=True, p=omega[at1, st1])
+    st1_samples = np.random.choice(n_s, size=n_root, replace=True, p=T[st, at1])
+    ot1_samples = [np.random.choice(n_o, size=n_root, replace=True,
+                                    p=omega[at1, st1])
                    for st1 in st1_samples]
-    return np.flatten(ot1_samples)
+    return np.array(ot1_samples).flatten()
+
+
+def sample_psi(T, omega, n_root=100):
+    (n_a, n_s, n_o) = omega.shape
+    res = np.empty((n_s, n_a, n_o))
+
+    for (st, at1, ot1), _ in np.ndenumerate(res):
+        ot1_samples = psi_samples(st, at1, T, omega, n_root=n_root)
+
+        res[st, at1, ot1] = np.mean(ot1_samples == ot1)
+
+    return res
